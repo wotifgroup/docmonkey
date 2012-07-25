106918d6d593628eafb942f9f6aa02a70f48e8f3
Brett Dargan <brett.dargan@gmail.com>
2012-07-26 09:56:03 +1000

    stock standard export is working, output to png

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # Introduction
 
-The Realconfu (Realities and concerns collide) application was developed to generate documentaiton. From a mix of automated and personal annotations to generate and public documentation.
+The Realconfu (Realities and concerns collide) application was developed to generate documentation. From a mix of automated and personal annotations to generate and public documentation.
 
 The drop wizard example application was developed to, as its name implies, provide examples of some of the features
 present in drop wizard.
 
 # Overview
+
+# Quick Start
+- Startup Omnigraffle 5
+- Run the Application (see below)
+- curl -XPOST -H"Content-type: application/json" localhost:8080/generate -d@src/main/resources/sample.json
+
+This will create a really simple diagram,
+
+If it doesn't look like the picture, then check the default template configuration in Omnigraffle. The auto layout features of Omnigraffle are needed.
+
+Html pages with pngs are now accessible via: (TBD)
+
 
 # Running The Application
 
@@ -18,3 +30,7 @@ To try the example application run the following commands.
 * To run the server run.
 
         java -jar target/dropwizard-realconfu-0.1.0-SNAPSHOT.jar server example.yml
+
+
+* Acknowledgements
+- good old posts in og forums: sunnysight 2009
--- src/main/java/com/wotifgroup/realconfu/Graph2Applescript.java
+++ src/main/java/com/wotifgroup/realconfu/Graph2Applescript.java
@@ -11,7 +11,7 @@ public class Graph2Applescript {
         StringBuffer sb = new StringBuffer();
 
         sb.append("tell application \"OmniGraffle Professional 5\"\n");
-        sb.append("	set myDocument to (make new document at end of documents with properties {name:\"test\", template: \"template_1\"})\n");
+        sb.append("	set theDocument to (make new document at end of documents with properties {name:\"test\", template: \"template_1\"})\n");
         sb.append("  tell canvas of front window\n");
 
 
@@ -19,11 +19,13 @@ public class Graph2Applescript {
         sb.append(addLinks(graph));
 
         sb.append(autolayout());
-        sb.append("\tsave myDocument\n");
-//        sb.append(export_png());
+        sb.append("\tsave theDocument\n");
         sb.append("end tell\n");
         sb.append("end tell\n");
 
+        sb.append("tell application \"OmniGraffle Professional 5\"\n");
+        sb.append(export_png(true));
+        sb.append("end tell\n");
 //        check with finder if the file exists, iterate till we find one.
 
         return sb.toString();
@@ -106,65 +108,77 @@ public class Graph2Applescript {
                 "end tell\n";
         return remove_file;
     }
-    public String export_png() {
-        String export = "property exportFileExtension : \"png\"\n" +
-                "property ADD_CANVAS_NUMBER : true\n" +
-                "-- End of Settings\n" +
+
+    public String export_png2() {
+        String export = "--Get the current document for later use \n" +
+                "set currentDocument to document of front window\n" +
                 "\n" +
-                "\tset theDocument to front document\n" +
-                "\tset _path to path of theDocument\n" +
+                "--Get a list of the canvases\n" +
+                "set theCanvases to every canvas of currentDocument\n" +
+                "\n" +
+                "--Use a counter for unique naming\n" +
+                "set counter to 0\n" +
+                "\n" +
+                "--Loop over each canvas\n" +
+                "repeat with aCanvas in theCanvases\n" +
+                "--Make sure that the current canvas is displayed \n" +
+                "--(export of currently selected only works in the displayed window\n" +
+                "set canvas of front window to aCanvas\n" +
+                "\n" +
+                "--get a list of groups for this canvas\n" +
+                "set theGroups to every graphic of aCanvas\n" +
+                "get theGroups\n" +
+                "\n" +
+                "--loop over each group/graphic\n" +
+                "repeat with aGroup in theGroups\n" +
+                "get aGroup\n" +
+                "\n" +
+                "--Set the selection of the window and save / export\n" +
+                "set selection of front window to {aGroup}\n" +
+                "save currentDocument in POSIX file (export_folder & filePrefix & (counter) & \".png\")\n" +
+                "set counter to counter + 1\n" +
+                "\n" +
+                "end repeat\n" +
+                "end repeat\n" +
+                "\n";
+
+        return export;
+    }
+
+    public String export_png(Boolean add_canvas_number_suffix) {
+        String fileExtension = "png";
+        String export = "set theDocument to front document\n\tset _path to path of theDocument\n" +
+                "\t\n" +
+                "\t-- if the file's being actively edited, a random object may be selected when we come to \n" +
+                "\t-- do the export, and with an object selected the export will default to 'selected graphics'  - oops!\n" +
+                "\t-- sooo, override the 'default' (and return it later)\n" +
+                "\tset oldAreaType to area type of current export settings\n" +
+                "\tset area type of current export settings to all graphics\n" +
                 "\t\n" +
                 "\tset area type of current export settings to current canvas\n" +
                 "\tset draws background of current export settings to false\n" +
                 "\tset include border of current export settings to false\n" +
                 "\t\n" +
+                "\tset export_folder to (POSIX path of \"/Users/Brett/work/tmp/export\")\n" +
+                "\tlog \"export_folder: \" & export_folder\n" +
                 "\t\n" +
-                "\ttell application \"Finder\"\n" +
-                "\t\tset {theFilename, _extension, _ishidden} to the \n" +
-                "\t\t\t{displayed name, name extension, extension hidden} \n" +
-                "\t\t\t\tof ((the _path as POSIX file) as alias)\n" +
-                "\tend tell\n" +
-                "\tif (_extension ­ missing value) then\n" +
-                "\t\tset theFilename to texts 1 thru -((length of _extension) + 2) of theFilename\n" +
-                "\tend if\n" +
+                "\t--if folder_exists(export_folder) then\n" +
+                "\t--\tlog \"export_folder already exists: \" & export_folder\n" +
+                "\t--end if\n" +
                 "\t\n" +
-                "\tset export_folder to (choose folder with prompt \"Pick the destination folder\") as string\n" +
-                "\tset export_folder to export_folder & theFilename & \":\"\n" +
-                "\t\n" +
-                "\t-- Create folder if does not exist, remove it otherwise\n" +
-                "\t-- Shell script should not be executed inside tell application block\n" +
-                "\ttell me\n" +
-                "\t\tif file_exists(export_folder) of me then\n" +
-                "\t\t\ttry\n" +
-                "\t\t\t\tdisplay alert \"The file already exists. Do you want to replace it?\" buttons {\"Cancel\", \"Erase\"} cancel button 1\n" +
-                "\t\t\ton error errText number errNum\n" +
-                "\t\t\t\tif (errNum is equal to -128) then\n" +
-                "\t\t\t\t\treturn\n" +
-                "\t\t\t\tend if\n" +
-                "\t\t\tend try\n" +
-                "\t\t\t\n" +
-                "\t\t\t-- Delete the folder\n" +
-                "\t\t\tdo shell script \"rm -rf \" & quoted form of POSIX path of export_folder\n" +
-                "\t\t\t\n" +
-                "\t\telse\n" +
-                "\t\t\t-- Create the folder\n" +
-                "\t\t\tdo shell script \"mkdir -p \" & quoted form of POSIX path of export_folder\n" +
-                "\t\tend if\n" +
-                "\tend tell\n" +
-                "\t\n" +
+                "\tset theFilename to name of theDocument\n" +
+                "\tset exportFilename to export_folder & \"/\" & theFilename & \".png\"\n" +
+                "\tlog \"export to \" & exportFilename\n" +
                 "\tset canvasCount to count of canvases of theDocument\n" +
                 "\t\n" +
                 "\tset canvasNumber to 1\n" +
                 "\trepeat with theCanvas in every canvas of theDocument\n" +
-                "\t\t-- Activate the canvas\n" +
                 "\t\tset canvas of front window to theCanvas\n" +
                 "\t\tset canvas_name to name of theCanvas\n" +
                 "\t\tset layerCount to count of layers of theCanvas\n" +
                 "\t\t\n" +
-                "\t\t-- Prepare filename\n" +
                 "\t\tset canvasFilename to \"\"\n" +
-                "\t\tset canvasFilename to \"\"\n" +
-                "\t\tif ADD_CANVAS_NUMBER then\n" +
+                "\t\tif " + add_canvas_number_suffix + " then\n" +
                 "\t\t\tset canvasFilename to canvasNumber & \"- \"\n" +
                 "\t\tend if\n" +
                 "\t\tset canvasFilename to canvasFilename & canvas_name\n" +
@@ -186,30 +200,26 @@ public class Graph2Applescript {
                 "\t\t\tend if\n" +
                 "\t\tend repeat\n" +
                 "\t\t\n" +
-                "\t\t-- Export each layer\n" +
                 "\t\trepeat with layerNumber from 1 to layerCount\n" +
                 "\t\t\tset theLayer to layer layerNumber of theCanvas\n" +
                 "\t\t\t\n" +
                 "\t\t\tif (theLayer is prints) and (class of theLayer is not shared layer) then\n" +
                 "\t\t\t\tset layer_name to name of theLayer as string\n" +
-                "\t\t\t\tset filename to canvasFilename & \" - \" & layer_name & \".\" & exportFileExtension\n" +
-                "\t\t\t\tset exportFilename to export_folder & filename\n" +
+                "\t\t\t\tset filename to canvasFilename & \" - \" & layer_name & \"." + fileExtension + "\"\n" +
+                "\t\t\t\tset exportFilename to export_folder & \"/\" & theFilename & filename\n" +
+                "\t\t\t\t--\t\t\t\tset exportFilename to export_folder & filename\n" +
+                "\t\t\t\tlog \"exportFilename: \" & exportFilename\n" +
                 "\t\t\t\t\n" +
                 "\t\t\t\t-- show the layer, export, then hide the layer\n" +
                 "\t\t\t\tif character 1 of layer_name is not \"*\" then\n" +
                 "\t\t\t\t\tset visible of theLayer to true\n" +
-                "\t\t\t\t\tsave theDocument in file exportFilename\n" +
+                "\t\t\t\t\tsave theDocument in exportFilename\n" +
                 "\t\t\t\t\tset visible of theLayer to false\n" +
                 "\t\t\t\tend if\n" +
-                "\t\t\t\t\n" +
                 "\t\t\tend if\n" +
-                "\t\t\t\n" +
                 "\t\tend repeat\n" +
-                "\t\t\n" +
                 "\t\tset canvasNumber to canvasNumber + 1\n" +
-                "\t\t\n" +
-                "\tend repeat\n" +
-                "end tell";
+                "\tend repeat\n";
         return export;
     }
 
